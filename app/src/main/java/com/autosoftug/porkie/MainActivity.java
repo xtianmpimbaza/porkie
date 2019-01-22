bdb716b4441e451b79308a6c9b5fcf9bad11a326
christian mpimbaza <xtianm4@gmail.com>
2019-01-22 23:28:40 +0300

    new order fragment

--- app/src/main/java/com/autosoftug/porkie/MainActivity.java
+++ app/src/main/java/com/autosoftug/porkie/MainActivity.java
@@ -77,8 +77,6 @@ public class MainActivity extends AppCompatActivity
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
 
-
-
     }
 
     @Override
